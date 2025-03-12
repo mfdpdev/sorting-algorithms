48abbe99bc9a69e28005bd5e5d55afc9d07a87bf
lord <danipramoedya2002@gmail.com>
2025-03-12 14:35:46 +0700

    Add Selection Sort implementation and test cases

--- src/sorting.py
+++ src/sorting.py
@@ -4,3 +4,12 @@ def bubble_sort(data):
             if(data[i] > data[j]):
                 data[j], data[i] = data[i], data[j]
     return data
+
+def selection_sort(data):
+    for i, _ in enumerate(data):
+        min = data[i]
+        for j in range(1 + i, len(data)):
+            if(data[j] < min):
+                min = data[j]
+                data[i], data[j] = min, data[i] 
+    return data
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,6 +1,12 @@
-from src.sorting import bubble_sort
+from src.sorting import bubble_sort, selection_sort
 
 def test_bubble_sort():
     assert bubble_sort([4,1,2,5,3]) == [1,2,3,4,5]
     assert bubble_sort([4,5,3,1,2]) == [1,2,3,4,5]
     assert bubble_sort([2,1,5,3,4]) == [1,2,3,4,5]
+
+def test_selection_sort():
+    assert selection_sort([4,1,2,5,3]) == [1,2,3,4,5]
+    assert selection_sort([4,5,3,1,2]) == [1,2,3,4,5]
+    assert selection_sort([2,1,5,3,4]) == [1,2,3,4,5]
+
