c7f9251cb7f0cb5cd9d1ef2c0fbc13b5b04f0a4f
lord <danipramoedya2002@gmail.com>
2025-03-13 13:35:48 +0700

    Add Insertion Sort implementation and test cases

--- src/sorting.py
+++ src/sorting.py
@@ -1,4 +1,5 @@
 def bubble_sort(data):
+    if len(data) <= 0: return []
     for i, _ in enumerate(data):
         for j in range(1 + i, len(data)):
             if(data[i] > data[j]):
@@ -6,10 +7,25 @@ def bubble_sort(data):
     return data
 
 def selection_sort(data):
+    if len(data) <= 0: return []
     for i, _ in enumerate(data):
-        min = data[i]
+        min = i
         for j in range(1 + i, len(data)):
-            if(data[j] < min):
-                min = data[j]
-                data[i], data[j] = min, data[i] 
+            if(data[j] < data[min]):
+                min = j
+        data[i], data[min] = data[min], data[i] 
     return data
+
+def insertion_sort(data):
+    if len(data) <= 0: return []
+    for i in range(1, len(data)):
+        key = data[i]
+        j = i - 1
+
+        while j >= 0 and data[j] > key:
+            data[j + 1] = data[j]
+            j -= 1
+
+        data[j + 1] = key
+    return data
+
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,12 +1,19 @@
-from src.sorting import bubble_sort, selection_sort
+from src.sorting import bubble_sort, selection_sort, insertion_sort
 
 def test_bubble_sort():
+    assert bubble_sort([]) == []
     assert bubble_sort([4,1,2,5,3]) == [1,2,3,4,5]
     assert bubble_sort([4,5,3,1,2]) == [1,2,3,4,5]
     assert bubble_sort([2,1,5,3,4]) == [1,2,3,4,5]
 
 def test_selection_sort():
+    assert selection_sort([]) == []
     assert selection_sort([4,1,2,5,3]) == [1,2,3,4,5]
     assert selection_sort([4,5,3,1,2]) == [1,2,3,4,5]
     assert selection_sort([2,1,5,3,4]) == [1,2,3,4,5]
 
+def test_insertion_sort():
+    assert insertion_sort([]) == []
+    assert insertion_sort([4,1,2,5,3]) == [1,2,3,4,5]
+    assert insertion_sort([4,5,3,1,2]) == [1,2,3,4,5]
+    assert insertion_sort([2,1,5,3,4]) == [1,2,3,4,5]
