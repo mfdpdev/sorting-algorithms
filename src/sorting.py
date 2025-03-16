41958efe76740b1d85128b74ca64bf35876d4f30
lord <danipramoedya2002@gmail.com>
2025-03-16 15:24:24 +0700

    Add merge sort implementation and test cases

--- src/sorting.py
+++ src/sorting.py
@@ -29,3 +29,37 @@ def insertion_sort(data):
         data[j + 1] = key
     return data
 
+def merge_sort(data):
+    if len(data) > 1:
+        mid = len(data) // 2
+
+        left_half = data[:mid]
+        right_half = data[mid:]
+
+        merge_sort(right_half) 
+        merge_sort(left_half) 
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                data[k] = left_half[i]
+                i += 1
+            else:
+                data[k] = right_half[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left_half):
+            data[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            data[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return data
+
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,4 +1,4 @@
-from src.sorting import bubble_sort, selection_sort, insertion_sort
+from src.sorting import bubble_sort, merge_sort, selection_sort, insertion_sort
 
 def test_bubble_sort():
     assert bubble_sort([]) == []
@@ -17,3 +17,10 @@ def test_insertion_sort():
     assert insertion_sort([4,1,2,5,3]) == [1,2,3,4,5]
     assert insertion_sort([4,5,3,1,2]) == [1,2,3,4,5]
     assert insertion_sort([2,1,5,3,4]) == [1,2,3,4,5]
+
+def test_merge_sort():
+    assert merge_sort([4,1,2,6,5,3]) == [1,2,3,4,5,6]
+    assert merge_sort([4,5,3,1,2]) == [1,2,3,4,5]
+    assert merge_sort([2,1,5,3,4]) == [1,2,3,4,5]
+    assert merge_sort([1]) == [1]
+    assert merge_sort([]) == []
